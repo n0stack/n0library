59ff468c7bb417b77e81fe15afaf09e4b7e5c3f1
yatuhashi kei <keyansuiya@gmail.com>
2017-09-27 21:38:36 +0900

    add log

---
+++ utils/n0corelog.py
@@ -0,0 +1,43 @@
+from logging import getLogger, INFO, WARNING, ERROR, DEBUG, Formatter
+from logging.handlers import RotatingFileHandler as RFH
+from logging import StreamHandler as SH
+from datetime import datetime
+import socket
+import sys
+
+
+class LogInit():
+
+    def __init__(self, path, name, level, stdout, fileout) -> None:
+        """
+        Args:
+            path (str): fileout path
+            name(str): filename
+            level(str): "info" or "warning" or "error" or "debug"
+            stdout(bool): True or False
+            fileout(bool): True or False
+
+        Example:
+            >>> from n0library.utils.n0corelog import LogInit
+            >>> log = LogInit("./log/test/", "test.log", "debug", True, True).logger
+            >>> log.info("tester")
+        """
+        self.logger = getLogger(__name__)
+        times = str(datetime.now().strftime("%Y:%m:%d-%H:%M:%S"))
+        filepath = str(path)
+        filename = str(name)
+        host = socket.gethostname()
+        log_fmt = "time:%(asctime)s \tseverity:[%(levelname)s] \tmessage:%(message)s  "
+        levels = {"info": INFO, "warning": WARNING, "error": ERROR, "debug": DEBUG}
+        if(stdout):
+            stdout_handler = SH(sys.stdout)
+            stdout_handler.setFormatter(Formatter(log_fmt))
+            stdout_handler.setLevel(levels[level])
+            self.logger.addHandler(stdout_handler)
+        if(fileout):
+            file_handler = RFH(filepath + host + "-" + times + "-" + filename, 'a+', 100000, 100)
+            file_handler.setFormatter(Formatter(log_fmt))
+            file_handler.level = levels[level]
+            self.logger.addHandler(file_handler)
+        self.logger.setLevel(levels[level])
+        self.logger.debug('Init')
