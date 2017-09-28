cd05f844babdda8eccb2c620f174d906a22d9f8c
yatuhashi kei <keyansuiya@gmail.com>
2017-09-28 20:48:21 +0900

    **kwargs

--- n0corelog.py
+++ n0corelog.py
@@ -8,19 +8,19 @@ import sys
 
 class LogInit():
 
-    def __init__(self, filepath, filename, level, stdout, fileout):
-        # type: (str, str, str, bool, bool) -> None
+    def __init__(self, stdout, fileout, **kwargs):
+        # type: (bool, bool, **str) -> None
         """
         Args:
-            path (str): fileout path
-            name(str): filename
-            level(str): "info" or "warning" or "error" or "debug"
             stdout(bool): True or False
             fileout(bool): True or False
+            filepath (str): fileout path
+            filename(str): filename
+            level(str): "info" or "warning" or "error" or "debug"
 
         Example:
             >>> from n0library.n0corelog import LogInit
-            >>> log = LogInit("./log/test/", "test.log", "debug", True, True).logger
+            >>> log = LogInit(True, True, filepath="./log/test/", filename="test.log", level="debug").logger
             >>> log.info("tester")
         """
         self.logger = getLogger(__name__)
@@ -31,12 +31,12 @@ class LogInit():
         if(stdout):
             stdout_handler = SH(sys.stdout)
             stdout_handler.setFormatter(Formatter(log_fmt))
-            stdout_handler.setLevel(levels[level])
+            stdout_handler.setLevel(levels[kwargs["level"]])
             self.logger.addHandler(stdout_handler)
         if(fileout):
-            file_handler = RFH(filepath + host + "-" + times + "-" + filename, 'a+', 100000, 100)
+            file_handler = RFH(kwargs["filepath"] + host + "-" + times + "-" + kwargs["filename"], 'a+', 100000, 100)
             file_handler.setFormatter(Formatter(log_fmt))
-            file_handler.level = levels[level]
+            file_handler.level = levels[kwargs["level"]]
             self.logger.addHandler(file_handler)
-        self.logger.setLevel(levels[level])
+        self.logger.setLevel(levels[kwargs["level"]])
         self.logger.debug('Init')
