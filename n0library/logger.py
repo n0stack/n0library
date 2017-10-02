a27f8aba8efe522a11c3b53b0878d6fc45a360b4
yatuhashi kei <keyansuiya@gmail.com>
2017-10-03 00:35:29 +0900

    fix option & add annotation

--- n0library/logger.py
+++ n0library/logger.py
@@ -1,4 +1,5 @@
-from logging import getLogger, INFO, WARNING, ERROR, DEBUG, Formatter
+from typing import List, Set, Dict, Tuple, Text, Optional, Any  # NOQA
+from logging import getLogger, INFO, WARNING, ERROR, DEBUG, Formatter, Logger as LoggerType  # NOQA
 from logging.handlers import RotatingFileHandler as RFH
 from logging import StreamHandler as SH
 from datetime import datetime
@@ -6,38 +7,54 @@ import socket
 import sys
 
 
-class Logger(object):
-    Logfmt = "time:%(asctime)s \tseverity:[%(levelname)s] \tmessage:%(message)s  "
-    Levels = {"info": INFO, "warning": WARNING, "error": ERROR, "debug": DEBUG}
+class Logger():
+    Logfmt = "time:%(asctime)s \tseverity:[%(levelname)s] \tmessage:%(message)s  "  # type: str
+    Levels = {"info": INFO, "warning": WARNING, "error": ERROR, "debug": DEBUG}  # type: dict
 
-    def __init__(self, stdout, fileout, name='root', **kwargs):
-        # type: (str, bool, bool, **str) -> None
+    def __init__(self, name='', stdout=False, level="debug", **kwargs):
+        # type: (str, bool, str, **str) -> None
         """
         Args:
+            logger(str): logger name
             stdout(bool): True or False
-            fileout(bool): True or False
+            level(str): "info" or "warning" or "error" or "debug"
             filepath (str): fileout path
             filename(str): filename
-            level(str): "info" or "warning" or "error" or "debug"
 
         Example:
             >>> from n0library.logger import Logger
-            >>> log = Logger(True, True, 'test', filepath="./log/test/", filename="test.log", level="debug").logger
+            >>> log = Logger('test', True, filepath="./log/test/", filename="test.log", level="debug")
             >>> log.info("tester")
         """
-        self.logger = getLogger(name)
-        times = str(datetime.now().strftime("%Y:%m:%d-%H:%M:%S"))
-        host = socket.gethostname()
+        self.logger = getLogger(name)  # type: LoggerType
+        times = str(datetime.now().strftime("%Y:%m:%d-%H:%M:%S"))  # type: str
+        host = socket.gethostname()  # type: str
 
         if(stdout):
-            stdout_handler = SH(sys.stdout)
+            stdout_handler = SH(sys.stdout)  # type: SH
             stdout_handler.setFormatter(Formatter(Logger.Logfmt))
-            stdout_handler.setLevel(Logger.Levels[kwargs["level"]])
+            stdout_handler.setLevel(Logger.Levels[level])
             self.logger.addHandler(stdout_handler)
-        if(fileout):
-            file_handler = RFH(kwargs["filepath"] + host + "-" + times + "-" + kwargs["filename"], 'a+', 100000, 100)
+        if("filepath" in kwargs):
+            file_handler = RFH(kwargs["filepath"] + host + "-" + times + "-" + kwargs["filename"], 'a+', 100000, 100)  # type: RFH
             file_handler.setFormatter(Formatter(Logger.Logfmt))
-            file_handler.level = Logger.Levels[kwargs["level"]]
+            file_handler.level = Logger.Levels[level]
             self.logger.addHandler(file_handler)
-        self.logger.setLevel(Logger.Levels[kwargs["level"]])
+        self.logger.setLevel(Logger.Levels[level])
         self.logger.debug('Init')
+
+    def info(self, msg, extra=None):
+        # type: (str, Dict[str, Any]) -> None
+        self.logger.info(msg, extra=extra)
+
+    def error(self, msg, extra=None):
+        # type: (str, Dict[str, Any]) -> None
+        self.logger.error(msg, extra=extra)
+
+    def debug(self, msg, extra=None):
+        # type: (str, Dict[str, Any]) -> None
+        self.logger.debug(msg, extra=extra)
+
+    def warn(self, msg, extra=None):
+        # type: (str, Dict[str, Any]) -> None
+        self.logger.warn(msg, extra=extra)
