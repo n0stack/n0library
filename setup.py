7d7c7408d103b7a535105b2864be48eb9f3d2110
h-otter <h-otter@outlook.jp>
2017-10-09 10:34:05 +0900

    check __name__ in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
-    readme = f.read()
+if __name__ == "__main__":
+    with open('README.md') as f:
+        readme = f.read()
 
-setup(
-    name='n0library',
-    version='0.0.0',
-    description='n0stack common library',
-    long_description=readme,
-    url='https://github.com/n0stack/n0library',
-    author='yatuhashi kei',
-    author_email='keyansuiya@gmail.com',
-    packages=find_packages()
-)
+    setup(
+        name='n0library',
+        version='0.0.0',
+        description='n0stack common library',
+        long_description=readme,
+        url='https://github.com/n0stack/n0library',
+        author='yatuhashi kei',
+        author_email='keyansuiya@gmail.com',
+        packages=find_packages()
+    )
